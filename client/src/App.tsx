9829f85f407b70baa999f111115d8584484240cb
Yuka Hiarayama <>
2022-08-06 19:08:44 +0900

    ファイルの保存

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import { SendButton } from './SendButton';
 
 function App() {
+    const [postFileData, setPostFileData] = useState({})
+    const changeUploadFile = async (event:any) => {
+      const { name, files } = event.target;
+      setPostFileData({
+        ...postFileData,
+        [name]: files[0],
+      });
+      event.target.value = '';
+      console.log(postFileData)
+    };
+
   return (
     <div className="App" style={{marginTop: "100px"}}>
+
       <header className="App-header">
         <p>
           あなたのパワーポイントをアップロードしていただくと、<br></br>
           それに合ったスライドをお作りします。
         </p>
-        <Button variant="contained">SUBMIT</Button>
+        <SendButton onChange={changeUploadFile} name="image">GO!</SendButton>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
 
 
 
---
+++ client/src/SendButton.tsx
@@ -0,0 +1,24 @@
+import Button from '@mui/material/Button';
+
+export const SendButton = (props:any) => {
+    return(
+        <label htmlFor={`upload-button-${props.name}`}>
+        <input style = {{display:"none"}}
+        accept="image/*"
+        id={`upload-button-${props.name}`}
+        name={props.name}
+        multiple
+        type="file"
+        onChange={props.onChange}
+        />
+        <Button variant="contained" component="span" >{props.children}</Button> 
+        </label>
+    )
+}
+
+    
+
+
+
+
+
