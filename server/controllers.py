88cfb5124b4b607eb70b0c813444b29eeaab6d8f
Nameless <tanisyun0331@icloud.com>
2022-08-07 12:09:46 +0900

    template のダウンロードを追加

--- server/controllers.py
+++ server/controllers.py
@@ -1,7 +1,11 @@
 from sys import prefix
 from fastapi import FastAPI
 from router.files import router as filesRouter
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title='pregen', prefix='/api')
+
+# /static/template.pptx : テンプレートダウンロード用
+app.mount('/static', StaticFiles(directory="tmp/data"), name='files')
 
 app.include_router(filesRouter)
