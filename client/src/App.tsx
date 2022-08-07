7287985a18580da4208468c88fbca8d9dea663f2
Nameless <tanisyun0331@icloud.com>
2022-08-07 15:10:52 +0900

    フロントと api を接続

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,81 +1,89 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import logo1 from './download 2.jpg';
-import logo2 from './logo.png';
-import './App.css';
-import Button from '@mui/material/Button';
-import { SendButton } from './SendButton';
-import axios from './utils/http_client';
-import Link from '@mui/material/Link';
-import { Download } from '@mui/icons-material';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import logo1 from "./download 2.jpg";
+import logo2 from "./logo.png";
+import "./App.css";
+import Button from "@mui/material/Button";
+import { SendButton } from "./SendButton";
+import axios from "./utils/http_client";
+import Link from "@mui/material/Link";
+import { Download } from "@mui/icons-material";
 
 function App() {
-    const [postFileData, setPostFileData] = useState<any>({})
-    const [filename,setFilename] = useState("")
-    const changeUploadFile = async (event:any) => {
-      const { name, files } = event.target;
-      setPostFileData({
-        ...postFileData,
-        [name]: files[0],
+  const [postFileData, setPostFileData] = useState<any>({});
+  const [filename, setFilename] = useState("");
+  const changeUploadFile = async (event: any) => {
+    const { name, files } = event.target;
+    setPostFileData({
+      ...postFileData,
+      [name]: files[0],
+    });
+    setFilename(files[0].name);
+
+    event.target.value = "";
+  };
+
+  const sendFile = async () => {
+    const formData = new FormData();
+    formData.append("file", postFileData.pptx ? postFileData.pptx : "");
+    const response = await axios
+      .post("/files/upload_pptx", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        responseType: "blob",
+      })
+      .then((res) => {
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "pregen.pptx");
+        document.body.appendChild(link);
+        link.click();
       });
-      setFilename(
-        files[0].name
-      )
+    console.log(response);
+  };
 
-      event.target.value = '';
-    };
+  const download = async () => {
+    await axios
+      .get("/static/template.pptx", {
+        responseType: "blob",
+      })
+      .then((res) => {
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "template.pptx");
+        document.body.appendChild(link);
+        link.click();
+      });
+  };
 
-const sendFile= async () => {
-  const formData = new FormData();
-formData.append(
-    'pptx',
-    postFileData.pptx ? postFileData.pptx : ''
-);
-const response = axios.post('/files/upload_pptx',formData,
-  {headers:{
-  'Content-Type': 'multipart/form-data',
-    },}
-  )};
-
- 
-  
   return (
-    <div className="App" style={{marginTop: "100px"}}>
-       
-       <header className="App-header">
-       <img className="logo" src={logo2} alt="picture" />
-          <p><Link underline="hover">こちらのテンプレート</Link>を編集して<br></br>
+    <div className="App" style={{ marginTop: "100px" }}>
+      <header className="App-header">
+        <img className="logo" src={logo2} alt="picture" />
+        <p>
+          <Link underline="hover" onClick={download}>
+            こちらのテンプレート
+          </Link>
+          を編集して<br></br>
           アップロードしていただくと、<br></br>
           それに合ったスライドを作成します。
         </p>
-        
+
         <div className="Button">
-        <SendButton onChange={changeUploadFile} name="pptx">アップロード</SendButton>
-        <Button variant="contained" disabled={!filename}>送信</Button>
-        {filename} 
+          <SendButton onChange={changeUploadFile} name="pptx">
+            アップロード
+          </SendButton>
+          <Button onClick={sendFile} variant="contained" disabled={!filename}>
+            送信
+          </Button>
+          <div>{filename}</div>
         </div>
-        <img src={logo1} alt="picture" />
-        </header>
-        
-     
+      </header>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
