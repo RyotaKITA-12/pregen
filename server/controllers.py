06d9c449aa103de8188675df773d4c63a7b82c71
Nameless <tanisyun0331@icloud.com>
2022-08-07 13:27:27 +0900

    cors対応

--- server/controllers.py
+++ server/controllers.py
@@ -2,10 +2,17 @@ from sys import prefix
 from fastapi import FastAPI
 from router.files import router as filesRouter
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='pregen', prefix='/api')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 # /static/template.pptx : テンプレートダウンロード用
-app.mount('/static', StaticFiles(directory="tmp/data"), name='files')
+app.mount('/static', StaticFiles(directory="tmp/pptx/data"), name='files')
 
 app.include_router(filesRouter)
